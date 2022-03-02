9ce75d7b79d5e47653be752f84cecd1ff39193d4
zhouquan3 <zhouquan3@yiche.com>
2022-03-02 18:54:24 +0800

    build

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 go_build() {
-    go build ./go03/go03.go
+    go build
 }
 
 docker_build() {
-    docker build -t zhqn.com:5000/umi-backend:1.0.0 ./go03
+    docker build -t zhqn.com:5000/umi-backend:1.0.0 .
 }
 docker_push() {
     docker push zhqn.com:5000/umi-backend:1.0.0
 }
+cd go03
 go_build && docker_build && docker_push
+cd ../
